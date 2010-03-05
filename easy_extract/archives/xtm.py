eaad2b232e2cf84999cc34865c5e1be1edc42cf8
Fantomas42 <fantomas42@gmail.com>
2010-03-05 11:27:36 +0100

    removing comment

--- easy_extract/archives/xtm.py
+++ easy_extract/archives/xtm.py
@@ -13,7 +13,6 @@ class XtmArchive(Archive):
 
     def _extract(self):
         new_filename = self.escape_filename(self.name)
-        # new_filename = self.get_command_filename(self.name)
         first_archive = self.get_command_filename(self.archives[0])
         
         os.system('dd if=%s skip=1 ibs=104 status=noxfer > %s 2>/dev/null' % \
