00b870e23a7c1f2581e5f9d58419525f3199b0f4
Fantomas42 <fantomas42@gmail.com>
2015-03-07 20:45:42 +0100

    Remove custom repair for hj_split archives

--- easy_extract/archives/hj_split.py
+++ easy_extract/archives/hj_split.py
@@ -12,18 +12,6 @@ class HJSplitArchive(Archive):
     archive_type = 'hj-split'
     ALLOWED_EXTENSIONS = EXTENSIONS
 
-    def check_and_repair(self, silent=False):
-        """Custom check and repair with medkits"""
-        if self.medkits:
-            options = silent and '-qq' or ''
-            root_medkit = self.get_command_filename(self.medkits[0])
-            medkit_extensions = self.get_command_filename(
-                '%s.0*' % self.name)
-            result = os.system('par2 r %s %s %s' % (
-                options, root_medkit, medkit_extensions))
-            return bool(not result)
-        return False
-
     def _extract(self):
         new_filename = self.escape_filename(self.name)
         first_archive = self.get_command_filename(self.archives[0])
